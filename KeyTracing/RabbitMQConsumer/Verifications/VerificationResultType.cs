d0dac8ee40853e98e5eb774ada954d529d5240ba
罗浩 <75821402@qq.com>
2017-05-10 17:43:38 +0800

    创建VerificationResultType.cs文件
    1.枚举成员默认是从0开始，自增1的整型常量

---
+++ KeyTracing/RabbitMQConsumer/Verifications/VerificationResultType.cs
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------------------
+// <copyright file="VerificationResultType.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RabbitMQ4Consumer.Verifications
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public enum VerificationResultType
+    {
+        Succeed,
+        Failed,
+    }
+}
